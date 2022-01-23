14cf088281500953581324b03a9acf9731bcbd27
Pruthvish Patel <skyshideout@Lucifers-Mac.local>
2022-01-23 06:09:39 -0500

    Initial commit

---
+++ src/main/java/com/example/w22st200492923assign1/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.w22st200492923assign1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.w22st200492923assign1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.w22st200492923assign1 to javafx.fxml;
+    exports com.example.w22st200492923assign1;
+}
