641981d6f9ab347830469685d590bb4f913ab917
sky1128 <pa.pruthvish@gmail.com>
2022-01-25 22:56:11 -0500

    First Commit

---
+++ src/main/java/com/example/w22st200492923assign1/Student.java
@@ -0,0 +1,40 @@
+package com.example.w22st200492923assign1;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int studentNumber;
+
+    public Student (String f1Name, String l1Name, int stNumber){
+
+        firstName = f1Name;
+        lastName = l1Name;
+        studentNumber = stNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+}
